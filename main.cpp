5aff077d9f7d69d9c6b4d10b435f969541c549aa
zmmarshall <zmmarshall@my.lonestar.edu>
2026-05-05 13:55:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 
 /*
  * Grade Calculator — Final Project
- * Author: [Your Name]
- * Course: COSC 1436, [Semester Year]
+ * Author: Z'Morion Marshall
+ * Course: COSC 1436, 2026 
  *
  * Concepts Demonstrated (Chapters 1-12)
  * -------------------------------------
